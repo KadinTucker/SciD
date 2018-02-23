65cd9c2bcea2fc563b05e13bb5f4bed47f963ffc
Kxoe <kxoethecat@gmail.com>
2018-02-22 20:21:01 -0700

    Moved unit and datum code to respective files

--- source/scid/data/Unit.d
+++ source/scid/data/Unit.d
@@ -1,1 +1,40 @@
 module scid.data.Unit;
+
+enum UnitPrefix {
+    PICO = 0.000000000001,
+    NANO = 0.000000001,
+    MICRO = 0.000001,
+    MILLI = 0.001,
+    CENTI = 0.01,
+    DECI = 0.1,
+    NONE = 1,
+    DEKA = 10,
+    HECTO = 100,
+    KILO = 1000,
+    MEGA = 1000000,
+    GIGA = 1000000000,
+    TERA = 1000000000000
+}
+
+enum UnitType {
+    GRAM = 'g',
+    METER = 'm',
+    LITER = 'L',
+    SECOND = 's',
+    NEWTON = 'N',
+    JOULE = 'J',
+    WATT = 'W',
+    KELVIN = 'K',
+    COULOMB = 'C',
+    VOLT = 'V',
+    AMPERE = 'A',
+    OHM = 'O'
+}
+
+struct Unit {
+
+    char name; ///A character representing this unit
+    UnitPrefix magnitude; ///The prefix which determines the unit's magnitude
+    int power; ///The multiplicity of the unit
+
+}
